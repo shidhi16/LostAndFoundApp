bf5231a85a5f4c9f4d49538c1b4de7051aa57a97
Nayna <nainavaghasiya9@gmail.com>
2019-07-06 17:00:16 -0400

    adding in signup

--- LostAndFoundApp/SignupViewController.swift
+++ LostAndFoundApp/SignupViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var txt_firstName: UITextField!
+    @IBOutlet weak var txt_lastName: UITextField!
+    
+    @IBOutlet weak var txt_dob: UITextField!
+    
+    @IBOutlet weak var txt_emailIDsignup: UITextField!
+    
+    @IBOutlet weak var txt_passwordSignup: UITextField!
+    
+    
+    @IBOutlet weak var txt_contactNo: UITextField!
+    
+    @IBOutlet weak var txt_address: UITextField!
+    
+    @IBOutlet weak var txt_city: UITextField!
+  
+    
+    
+    @IBAction func btn_save(_ sender: UIBarButtonItem)
+    {
+        let myStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "loginVC") as! LoginViewController
+        self.present(nextVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func txt_dobClick(_ sender: UITextField)
+    {
+    
+    }
+    @IBAction func btnBack(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func backbtn(_ sender: Any)
+    {
+        let myStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "loginVC") as! LoginViewController
+        self.present(nextVC, animated: true, completion: nil)
+    }
+    @IBAction func btn_Back(_ sender: Any) {
+       
+    
+    }
+    
+    @IBAction func btn_back(_ sender: UIBarButtonItem)
+    { let secVC = storyboard!.instantiateViewController(withIdentifier: "loginVC") as! LoginViewController
+        
+        self.navigationController?.pushViewController(secVC, animated: true)
+      
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
