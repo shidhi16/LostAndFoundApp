858dec1b851874b0b2ec3fe3aca538432f9e92ec
Nayna <nainavaghasiya9@gmail.com>
2019-07-08 13:17:35 -0400

    login class Remember me functionality added and background image added

--- LostAndFoundApp/LoginViewController.swift
+++ LostAndFoundApp/LoginViewController.swift
@@ -25,10 +25,54 @@ class LoginViewController: UIViewController {
         let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "signupVC") as! SignupViewController
         self.present(nextVC, animated: true, completion: nil)
     }
+    
+    
+    private func getRememberMeValues()
+    {
+        let userDefault = UserDefaults.standard
+        
+        if let email = userDefault.string(forKey: "userEmail")
+        {
+            txt_emailId.text = email
+            
+            if let pwd = userDefault.string(forKey: "userPassword")
+            {
+                txtPassword.text = pwd
+            }
+        }
+    }
     @IBAction func btnLogin(_ sender: UIButton)
     {
-        validateUser()
+        if self.txt_emailId.text == "admin@gmail.com" && self.txtPassword.text == "s3cr3t"
+        {
+            let userDefault = UserDefaults.standard
+            if switchRememberMe.isOn
+            {
+                
+                userDefault.setValue(txt_emailId.text, forKey: "userEmail")
+                userDefault.set(txtPassword.text, forKey: "userPassword")
+                let myStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "MenuVC") as! MenuViewController
+                self.present(nextVC, animated: true, completion: nil)
+            }
+            else
+            {
+                userDefault.removeObject(forKey: "userEmail")
+                userDefault.removeObject(forKey: "userPassword")
+            }
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Error", message: "Try again, User Email / Password Invalid", preferredStyle: .alert)
+            
+            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okButton)
+            
+            self.present(alert, animated: true)
+        }
     }
+    /*
         func validateUser()
         {
             if(txt_emailId.text == "admin@gmail.com" && txtPassword.text == "admin123")
@@ -58,10 +102,12 @@ class LoginViewController: UIViewController {
         
     
     
-    
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getRememberMeValues()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
         
     }
 
