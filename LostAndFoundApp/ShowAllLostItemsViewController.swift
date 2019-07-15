7aa69fd16e5ba65b965cc12738a43a398617d05b
NaynaV <nainavaghasiya9@gmail.com>
2019-07-15 14:52:53 -0400

    [Nayna] ShowLostItem VC added and Viewcontroller class added

---
+++ LostAndFoundApp/ShowAllLostItemsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ShowAllLostItemsViewController.swift
+//  LostAndFoundApp
+//
+//  Created by Nayna on 7/15/19.
+//  Copyright © 2019 Nayna. All rights reserved.
+//
+
+import UIKit
+
+class ShowAllLostItemsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    
+    
+    @IBOutlet weak var tbl_showLostItems: UITableView!
+    
+    @IBOutlet weak var cell_showLostItems: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+  
+
+}
--- LostAndFoundApp/Swift files/Item.swift
+++ LostAndFoundApp/Swift files/Item.swift
@@ -24,8 +24,6 @@ class Item
         self.itemName = String()
         self.description = String()
         
-       
-        
     }
     
     init(itemID:Int, itemCategory : String, itemName :String, description : String)
